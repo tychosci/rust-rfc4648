ca47d749ae6c8acf730652d0c05ca088cff9c216
Tycho Sci <tychosci@gmail.com>
2012-03-21 15:23:23 +0900

    add new stub

---
+++ base32n.rs
@@ -0,0 +1,31 @@
+//
+// base32n.rs - new base32 module
+//
+// The Base32 Alphabet
+//
+// Value Encoding  Value Encoding  Value Encoding  Value Encoding
+//     0 A             9 J            18 S            27 3
+//     1 B            10 K            19 T            28 4
+//     2 C            11 L            20 U            29 5
+//     3 D            12 M            21 V            30 6
+//     4 E            13 N            22 W            31 7
+//     5 F            14 O            23 X
+//     6 G            15 P            24 Y         (pad) =
+//     7 H            16 Q            25 Z
+//     8 I            17 R            26 2
+//
+// The "Extended Hex" Base 32 Alphabet
+//
+// Value Encoding  Value Encoding  Value Encoding  Value Encoding
+//     0 0             9 9            18 I            27 R
+//     1 1            10 A            19 J            28 S
+//     2 2            11 B            20 K            29 T
+//     3 3            12 C            21 L            30 U
+//     4 4            13 D            22 M            31 V
+//     5 5            14 E            23 N
+//     6 6            15 F            24 O         (pad) =
+//     7 7            16 G            25 P
+//     8 8            17 H            26 Q
+//
+
+// FIXME write
---
+++ base64n.rs
@@ -0,0 +1,47 @@
+//
+// base64n.rs - new base64 module
+//
+// The Base64 Alphabet
+//
+//  Value Encoding  Value Encoding  Value Encoding  Value Encoding
+//      0 A            17 R            34 i            51 z
+//      1 B            18 S            35 j            52 0
+//      2 C            19 T            36 k            53 1
+//      3 D            20 U            37 l            54 2
+//      4 E            21 V            38 m            55 3
+//      5 F            22 W            39 n            56 4
+//      6 G            23 X            40 o            57 5
+//      7 H            24 Y            41 p            58 6
+//      8 I            25 Z            42 q            59 7
+//      9 J            26 a            43 r            60 8
+//     10 K            27 b            44 s            61 9
+//     11 L            28 c            45 t            62 +
+//     12 M            29 d            46 u            63 /
+//     13 N            30 e            47 v
+//     14 O            31 f            48 w         (pad) =
+//     15 P            32 g            49 x
+//     16 Q            33 h            50 y
+//
+// The "URL and Filename safe" Base 64 Alphabet
+//
+// Value Encoding  Value Encoding  Value Encoding  Value Encoding
+//     0 A            17 R            34 i            51 z
+//     1 B            18 S            35 j            52 0
+//     2 C            19 T            36 k            53 1
+//     3 D            20 U            37 l            54 2
+//     4 E            21 V            38 m            55 3
+//     5 F            22 W            39 n            56 4
+//     6 G            23 X            40 o            57 5
+//     7 H            24 Y            41 p            58 6
+//     8 I            25 Z            42 q            59 7
+//     9 J            26 a            43 r            60 8
+//    10 K            27 b            44 s            61 9
+//    11 L            28 c            45 t            62 - (minus)
+//    12 M            29 d            46 u            63 _ (underline)
+//    13 N            30 e            47 v
+//    14 O            31 f            48 w
+//    15 P            32 g            49 x
+//    16 Q            33 h            50 y         (pad) =
+//
+
+// FIXME write
