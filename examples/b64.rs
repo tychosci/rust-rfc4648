07d1e73537fccc8c50edaa0e362d0ad428452155
Tycho Sci <tychosci@gmail.com>
2012-03-26 23:24:23 +0900

    examples: update b64.rs

--- examples/b64.rs
+++ examples/b64.rs
@@ -6,17 +6,19 @@ import encoding::base64;
 fn main(args: [str]) {
     let mut args = args;
     let binary = vec::shift(args);
-    let stdout = io::stdout(), stderr = io::stderr();
-    let b64 = base64::mk();
+    let stdout = io::stdout();
+    let stderr = io::stderr();
+
     if vec::len(args) < 2u {
         stderr.write_line(#fmt["Usage: %s <mode> <filename>", binary]);
         ret;
     }
+
     alt io::read_whole_file(args[1]) {
       result::ok(data) {
         let res = alt args[0] {
-          "encode" { b64.encode_bytes(data) }
-          "decode" { b64.decode_bytes(data) }
+          "encode" { base64::encode(data) }
+          "decode" { base64::decode(data) }
           _ { ret; }
         };
         stdout.write(res);
