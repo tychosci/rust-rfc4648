c10259ccba0af3cd192039c40d70f55d5c3612bf
Tycho Sci <tychosci@gmail.com>
2012-04-05 14:14:43 +0900

    url: Add url_unescape and query_unescape

--- url.rs
+++ url.rs
@@ -4,6 +4,8 @@
 
 import vec::len;
 
+export query_escape, query_unescape;
+
 enum enc_mode {
     query,
     path,
@@ -12,6 +14,23 @@ enum enc_mode {
 }
 
 fn query_escape(s: str) -> str { url_escape(s, query) }
+fn query_unescape(s: str) -> str { url_unescape(s, query) }
+
+#[inline(always)]
+fn ishex(c: u8) -> bool {
+    if 48u8 <= c && c <= 57u8 { true }
+    else if 65u8 <= c && c <= 90u8  { true }
+    else if 97u8 <= c && c <= 122u8 { true }
+    else { false }
+}
+
+#[inline(always)]
+fn unhex(c: u8) -> u8 {
+    if 48u8 <= c && c <= 57u8 { c - 48u8 }
+    else if 65u8 <= c && c <= 90u8  { c - 65u8 + 10u8 }
+    else if 97u8 <= c && c <= 122u8 { c - 97u8 + 10u8 }
+    else { fail "should be unreachable"; }
+}
 
 fn should_escape(c: u8, mode: enc_mode) -> bool {
     alt c {
@@ -91,6 +110,58 @@ fn url_escape(s: str, mode: enc_mode) -> str {
     str::from_bytes(vec::from_mut(ts))
 }
 
+fn url_unescape(s: str, mode: enc_mode) -> str {
+    let bs = str::bytes(s);
+    let src_length = len(bs);
+    let mut n = 0u;
+    let mut i = 0u;
+    let mut hasplus = false;
+
+    while i < src_length {
+        let c = bs[i];
+        if c == 37u8 {
+            n += 1u;
+            if i+2u >= src_length || !ishex(bs[i+1u]) || !ishex(bs[i+2u]) {
+                fail #fmt["Invalid URL escape: '%s'", s];
+            }
+            i += 3u;
+        } else if c == 43u8 {
+            hasplus = mode == query;
+            i += 1u;
+        } else {
+            i += 1u;
+        }
+    }
+
+    if n == 0u && !hasplus {
+        ret s;
+    }
+
+    let ts = vec::to_mut(vec::from_elem(src_length - 2u * n, 0u8));
+    let mut i = 0u;
+    let mut j = 0u;
+
+    while i < src_length {
+        let c = bs[i];
+        if c == 37u8 {
+            ts[j] = (unhex(bs[i+1u])<<4u) | unhex(bs[i+2u]);
+            j += 1u;
+            i += 3u;
+        } else if c == 43u8 {
+            if mode == query { ts[j] = 32u8; }
+            else { ts[j] = 43u8; }
+            j += 1u;
+            i += 1u;
+        } else {
+            ts[j] = c;
+            j += 1u;
+            i += 1u;
+        }
+    }
+
+    str::from_bytes(vec::from_mut(ts))
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -115,5 +186,13 @@ mod tests {
         assert query_escape("å") == "%C3%A5";
         assert query_escape("?") == "%3F";
         assert query_escape("あ") == "%E3%81%82";
+    }
+    #[test]
+    fn test_query_unescape() {
+        assert query_unescape("a") == "a";
+        assert query_unescape("a+z") == "a z";
+        assert query_unescape("%3f") == "?";
+        assert query_unescape("%C3%A5") == "å";
+        assert query_unescape("%E3%81%82") == "あ";
     }
 }
