d1cc849a7c050492f22c7c8af18b18c70ed8b087
Tycho Sci <tychosci@gmail.com>
2012-08-06 02:31:37 +0900

    match expr as rhs

--- examples/b64.rs
+++ examples/b64.rs
@@ -9,21 +9,18 @@ fn main(args: ~[~str]) {
     let stderr = io::stderr();
 
     if args.len() < 2u {
-        stderr.write_line(fmt!{"Usage: %s <mode> <filename>", binary});
+        stderr.write_line(fmt!("Usage: %s <mode> <filename>", binary));
         return;
     }
 
     match io::read_whole_file(args[2]) {
-        result::ok(data) => {
-            let res = match args[1] {
-                ~"encode" => data.encode(encoding::base64)
-              , ~"decode" => data.decode(encoding::base64)
-              , _         => return
-            };
-            stdout.write(res);
+        result::ok(data) => match args[1] {
+            ~"encode" => stdout.write(data.encode(encoding::base64))
+          , ~"decode" => stdout.write(data.decode(encoding::base64))
+          , _         => return
         }
         result::err(msg) => {
-            stderr.write_line(#fmt["Error: %s", msg]);
+            stderr.write_line(fmt!("Error: %s", msg));
         }
     }
 }
