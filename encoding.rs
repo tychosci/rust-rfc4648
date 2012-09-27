a8f61966474924c73e9b7c466f876d93a93e8121
Tycho Sci <tychosci@gmail.com>
2012-09-27 14:25:58 +0900

    Un-legacy-exports encoding

--- encoding.rs
+++ encoding.rs
@@ -1,48 +1,25 @@
 // encoding.rs
 
-#[legacy_exports];
-
 // import enum variants in misc.rs
-use Base16 = misc::Base16;
-use Base32 = misc::Base32;
-use Base64 = misc::Base64;
-use Base32Hex = misc::Base32Hex;
-use Base64Url = misc::Base64Url;
+pub use Base16 = misc::Base16;
+pub use Base32 = misc::Base32;
+pub use Base64 = misc::Base64;
+pub use Base32Hex = misc::Base32Hex;
+pub use Base64Url = misc::Base64Url;
 
 // import constants in misc/base{16,32,64}.rs
-use BASE16 = misc::base16::BASE16;
-use BASE32 = misc::base32::BASE32_STD;
-use BASE64 = misc::base64::BASE64_STD;
-use BASE32_HEX = misc::base32::BASE32_HEX;
-use BASE64_URL = misc::base64::BASE64_URL;
+pub use BASE16 = misc::base16::BASE16;
+pub use BASE32 = misc::base32::BASE32_STD;
+pub use BASE64 = misc::base64::BASE64_STD;
+pub use BASE32_HEX = misc::base32::BASE32_HEX;
+pub use BASE64_URL = misc::base64::BASE64_URL;
 
 // import structs in misc/base{16,32,64}.rs
-use Base16Writer = misc::base16::Base16Writer;
-use Base32Writer = misc::base32::Base32Writer;
-use Base64Writer = misc::base64::Base64Writer;
-use Base32Reader = misc::base32::Base32Reader;
-use Base64Reader = misc::base64::Base64Reader;
-
-export Base16;
-export Base32;
-export Base64;
-export Base32Hex;
-export Base64Url;
-export BASE16;
-export BASE32;
-export BASE64;
-export BASE32_HEX;
-export BASE64_URL;
-export Base16Writer;
-export Base32Writer;
-export Base64Writer;
-export Base32Reader;
-export Base64Reader;
-
-export Encode;
-export Decode;
-export Convert;
-export Codec;
+pub use Base16Writer = misc::base16::Base16Writer;
+pub use Base32Writer = misc::base32::Base32Writer;
+pub use Base64Writer = misc::base64::Base64Writer;
+pub use Base32Reader = misc::base32::Base32Reader;
+pub use Base64Reader = misc::base64::Base64Reader;
 
 pub trait Encode {
     fn encode(buf: &[u8]) -> ~[u8];
